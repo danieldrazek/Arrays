efdc425aaa6b9722b4a006259beab56b46dbab52
danieldrazek <daniel_2000@o2.pl>
2019-05-27 19:56:55 +0100

    ArrayEx2.java modified

--- ArrayEx2.java
+++ ArrayEx2.java
@@ -4,31 +4,28 @@ package array;
     
 public class ArrayEx2 {
     
-    public int biggestNumber(int[] numbers){   
-        
+    public int biggestNumber(int[] numbers){              
         int biggest = numbers[0];
         if(numbers[1] > biggest){
             biggest = numbers[1];
         }
-        else if(numbers[2] > biggest){
+        else(numbers[2] > biggest){
             biggest = numbers[2];
         }  
         return biggest;    
     }
     
     public int biggestNumber2(int[] numbers){
-        return Math.max(numbers[0], Math.max(numbers[1], numbers[2]));
-        
+        return Math.max(numbers[0], Math.max(numbers[1], numbers[2]));     
     }
     
     public static void main(String[] args) {
         
             ArrayEx2 excercise2 = new ArrayEx2();
-            int[] numbers = new int[]{12, 67, -7};
+            int[] numbers = new int[]{1, 2, 3};
             
             System.out.println("The bigger number is: " + excercise2.biggestNumber(numbers));
-            System.out.println("The bigger number is: " + excercise2.biggestNumber2(numbers));
-           
+            System.out.println("The bigger number is: " + excercise2.biggestNumber2(numbers));          
     }
 }
 
